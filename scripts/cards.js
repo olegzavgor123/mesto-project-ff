71e7aee873c594a763c17505dd9d2b4cf4e00252
olegzavgor <olegzavgor123@mail.ru>
2024-05-18 18:07:54 +0300

    2 commit

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt: "Картинка Архыза",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Картинка Челябинской области",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Картинка Иваново",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "Картинка Камчатки",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Картинка Холмгорского района",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "Картинка Байкала",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,22 @@
 const container = document.querySelector('.places__list');
 
-function templateCard (card){
+function createCard (name, link, alt, deleteCard){
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = card.link;
-  cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = alt;
+  cardElement.querySelector('.card__title').textContent = name;
   deleteCard(cardElement);
   return cardElement;
 }
 
-function deleteCard (item) {
+function deleteCard(item) {
   const deleteButton = item.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', () => {
     item.remove();
   })
 }
 
-
 initialCards.forEach((item) => {
-  container.append(templateCard(item));
+  container.append(createCard(item.name, item.link, item.alt, deleteCard));
 })
