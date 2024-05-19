a34134632965367d5299056171d5961df32f80eb
olegzavgor <olegzavgor123@mail.ru>
2024-05-19 21:20:59 +0300

    3 commit

--- scripts/index.js
+++ scripts/index.js
@@ -6,17 +6,24 @@ function createCard (name, link, alt, deleteCard){
   cardElement.querySelector('.card__image').src = link;
   cardElement.querySelector('.card__image').alt = alt;
   cardElement.querySelector('.card__title').textContent = name;
-  deleteCard(cardElement);
+  const deleteButton = cardElement.querySelector('.card__delete-button'); 
+  deleteButton.addEventListener('click', () => { deleteCard(cardElement) });
   return cardElement;
 }
 
 function deleteCard(item) {
-  const deleteButton = item.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', () => {
-    item.remove();
-  })
-}
+  item.remove();
+} 
+
 
 initialCards.forEach((item) => {
   container.append(createCard(item.name, item.link, item.alt, deleteCard));
 })
+
+
+// function deleteCard(item) {
+//   const deleteButton = item.querySelector('.card__delete-button');
+//   deleteButton.addEventListener('click', () => {
+//     item.remove();
+//   })
+// }
