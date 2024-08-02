7fee816b69613a2d7b22bd7ce19eaa34c73e1a0b
olegzavgor <olegzavgor123@mail.ru>
2024-08-02 15:45:35 +0300

    7 sprint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -97,3 +97,54 @@ function openPopImage(link, desc){
   openPopup(popupTypeImage);
 }
 
+
+
+//7 sprint
+
+//Проходим по всем формам
+function enableValidation() {
+  const formList = Array.from(document.forms);
+  formList.forEach((formElem)=> {
+    formElem.addEventListener('submit', (evt)=> {
+      evt.preventDefault();
+    });
+    setEventListener(formElem)
+  })
+}
+
+//Слушатель на каждый инпут
+function setEventListener(formElem) {
+  const inputList = Array.from(formElem.querySelectorAll('.popup__input'));
+  inputList.forEach((input)=> {
+    input.addEventListener('input', ()=> {
+      checkInputValidation(formElem, input);
+    })
+  })
+}
+
+//Проверка валидации инпутов
+function checkInputValidation(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage)
+  } else  {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+//Показать ошибку
+function showInputError(formElement, inputElement, errorMessage){
+  const errorElem = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElem.textContent = errorMessage;
+  inputElement.classList.add('form__input_type_error');
+  errorElem.classList.add('.form__input-error_active');
+}
+
+//Скрыть ошибку
+function hideInputError(formElement, inputElement) {
+  const errorElem = formElement.querySelector(`.${inputElement.id}-error`);
+  errorElem.textContent = '';
+  inputElement.classList.remove('form__input_type_error');
+  errorElem.classList.remove('.form__input-error_active');
+}
+
+enableValidation();
